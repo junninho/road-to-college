f84524c229f3c684305cab2aac4515039deb333a
jujut1998 <jthomas2016@bklawtech.com>
2015-07-31 12:51:20 -0400

    Added page input box

--- client/js/index.js
+++ client/js/index.js
@@ -3,3 +3,19 @@ function toggleScrubber(scrubber) {
             style.display = style.display === 'none' ? '' : 'none';
         })(document.getElementById(scrubber).style);
     }
+
+function changePage()
+{
+        var keyPressed = event.keyCode || event.which;
+
+        //if ENTER is pressed
+        if(keyPressed==13)
+        {
+            console.log("The user would like to go to page " + document.getElementById("pageInput").value);
+            keyPressed=null;
+        }
+        else
+        {
+            return false;
+        }
+}
